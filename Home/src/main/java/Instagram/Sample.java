38b179b91ec7fc11850b689de0fedfd2f9335cb7
root <root@ip-172-31-26-143.eu-north-1.compute.internal>
2024-11-08 09:47:41 +0000

    War

---
+++ Home/src/main/java/Instagram/Sample.java
@@ -0,0 +1,13 @@
+package Instagram;
+
+/**
+ * Hello world!
+ *
+ */
+public class Sample
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! I am Sravani" );
+    }
+}
